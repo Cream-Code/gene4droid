8877f131615be7362ec25c7e774ae448299f1e74
joseph <joekishore999@gmail.com>
2020-12-12 15:59:35 +0530

    Tool bar added with logo, search bar, search icon

--- app/src/main/java/com/joe/gene4droid/MainActivity.java
+++ app/src/main/java/com/joe/gene4droid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.joe.gene4droid;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
+import android.view.View;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -8,30 +10,39 @@ import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
     SwipeRefreshLayout swipe;
+    EditText in_url;
     String url;
+    ImageButton search_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        default home page - google
+        url="https://www.google.com";
         swipe= findViewById(R.id.swipe);
         swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                WebAction();
+                WebAction(url);
             }
         });
-        WebAction();
+        WebAction(url);
     }
-    public void WebAction(){
+//    enabling Java Script
+    @SuppressLint("SetJavaScriptEnabled")
+    public void WebAction(String url){
         webView=findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAppCacheEnabled(true);
-        webView.loadUrl("https://www.google.com");
+        webView.loadUrl(url);
         swipe.setRefreshing(true);
+
         webView.setWebViewClient(
                 new WebViewClient(){
                     @Override
@@ -47,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
-    
+    // Go back if back pressed
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()){
